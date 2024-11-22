804e4d44bc20accb191ac7aa4682838f1645061b
Rui Sa <rui-sa@users.noreply.github.com>
2024-11-22 12:52:33 +0000

    My first website

--- app.py
+++ app.py
@@ -73,9 +73,6 @@ params = {
     "passenger_count": passenger_count
 }
 
-
-full_url = requests.Request('GET', url, params=params).prepare().url
-print(f"Full URL being called: {full_url}")
 response = requests.get(url, params=params)
 fare = response.json().get('fare')
 print(fare)
@@ -83,26 +80,18 @@ print(fare)
 if st.button('Calculate'):
     st.write(fare)
 else:
-    st.write('I was not clicked 😞')
+    st.write('I was not clicked')
 
 
 # http://127.0.0.1:8000/predict?pickup_datetime=2014-07-06+19:18:00&pickup_longitude=-73.950655&pickup_latitude=40.783282&dropoff_longitude=-73.984365&dropoff_latitude=40.769802&passenger_count=2
 # http://127.0.0.1:8000/predict?pickup_datetime=2024-11-22+12%3A44&pickup_longitude=-73.950655&pickup_latitude=40.783282&dropoff_longitude=-73.984365&dropoff_latitude=40.769802&passenger_count=2
+
 # pickup_datetime: str,  # 2014-07-06 19:18:00
-
-
-    # """
-    # Make a single course prediction.
-    # Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
-    # Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
-    # """
+# pickup_longitude: float,    # -73.950655
+# pickup_latitude: float,     # 40.783282
+# dropoff_longitude: float,   # -73.984365
+# dropoff_latitude: float,    # 40.769802
+# passenger_count: int
     
-    # pickup_datetime = " ".join(pickup_datetime.split("+"))
-
-    # pickup_datetime: str,  # 2014-07-06 19:18:00
-    # pickup_longitude: float,    # -73.950655
-    # pickup_latitude: float,     # 40.783282
-    # dropoff_longitude: float,   # -73.984365
-    # dropoff_latitude: float,    # 40.769802
-    # passenger_count: int
-    
+# full_url = requests.Request('GET', url, params=params).prepare().url
+# print(f"Full URL being called: {full_url}")
